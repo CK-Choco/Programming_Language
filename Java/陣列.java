048c48ffab959874d9acfa81aa3f4961b859967f
HsiaoChoco <avatyu89@gmail.com>
2022-11-30 18:30:46 +0800

    Java

---
+++ Java/陣列.java
@@ -0,0 +1,10 @@
+public class 陣列 {
+    public static void main(String[] args)
+    {
+//        int student1=10, int student2=20, int student3=30, int student4=40, int student5=50, int student6=60, int student7=70, int student8=90, int student9=80, student10=100;
+         int[] x =new int[7];
+         x[0]=2;
+         System.out.println(x[0]);
+    
+    }
+}
