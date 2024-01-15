b0a9f63364a72cbc276ed31c7738f4bf69155645
HsiaoChoco <avatyu89@gmail.com>
2024-01-15 23:45:00 +0800

    Test JavaScript

---
+++ Test測試.js
@@ -0,0 +1,12 @@
+// ===========================
+// * Hello JavaScript
+// ? How are you?
+// ! I am Vscode
+// # Todo You
+// ^ fix code
+// ===========================
+
+// # 變數
+var errors= 0;
+// * 輸出文字+變數
+console.log('How are you? \n㇄I am JavaScript \n㇄fix me \n㇄error : '+errors);
